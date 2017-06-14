3e6a7d87088581f3aee8c5dc2522850dd8b2364b
AlexKott <alexey.kott@gmail.com>
2017-06-15 01:19:15 +0300

    make request like json

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ except ImportError:
 	print "У вас не установлен модуль requests. Установите модуль."
 import sys
 
-url = "https://requestb.in/"
+url = "https://requestb.in/1jx82j01"
 
 if len(sys.argv) == 1:
 	print 'Вы не указали файл. Укажите файл аргументом в команде "python3 script.py <filename>"'
@@ -21,7 +21,7 @@ with open(sys.argv[1], 'r') as csvfile:
 		for i in range(n):
 			params[args[i]] = row[i]
 		headers =  {'Content-type': 'application/json'}
-		r = req.post(url, params, headers = headers)
+		r = req.post(url, json = params, headers = headers)
 		if r.status_code != 200:
 			print "Error"
 			exit()
